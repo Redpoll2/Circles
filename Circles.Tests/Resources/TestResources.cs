c261d172036514be63ab226fc74ee7dc122f9f20
Redpoll <dredpoll02@gmail.com>
2021-03-04 19:19:22 +0500

    Add resources for testing

---
+++ Circles.Tests/Resources/TestResources.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Circles.Tests.Resources
+{
+    public static class TestResources
+    {
+        public static Stream GetResourceStream(string filename)
+        {
+            var asm = typeof(TestResources).Assembly;
+
+            return asm.GetManifestResourceStream(
+                $"{asm.GetName().Name}.Resources.{filename}");
+        }
+    }
+}
