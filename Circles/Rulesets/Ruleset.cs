2932ffcc8bf492807ff3502f4c3b180cd1427747
Redpoll <dredpoll02@gmail.com>
2021-03-04 19:18:53 +0500

    Add osu mods and rulesets (modes)

---
+++ Circles/Rulesets/Ruleset.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2021 02Redpoll. See the MIT license for full information
+
+namespace Circles.Rulesets
+{
+    public enum Ruleset : byte
+    {
+        Standart,
+        Taiko,
+        CatchTheBeat,
+        Mania
+    }
+}
