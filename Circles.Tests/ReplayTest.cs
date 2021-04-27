a334aca075190b068bfc25a986f94778e3f88a2a
Redpoll <dredpoll02@gmail.com>
2021-04-27 19:01:29 +0500

    Implement replay's load methods

---
+++ Circles.Tests/ReplayTest.cs
@@ -0,0 +1,32 @@
+﻿using Circles.Replays;
+using Circles.Rulesets;
+using Circles.Tests.Resources;
+using Xunit;
+
+namespace Circles.Tests
+{
+    public class ReplayTest
+    {
+        [Fact]
+        public void LoadHeader()
+        {
+            var replay = new Replay();
+
+            replay.Load(TestResources.GetResourceStream("aetrna - 07th Expansion - rog-unlimitation [AngelHoney] (2020-07-24) Osu.osr"));
+
+            Assert.Equal("aetrna", replay.Username);
+            Assert.Equal(Mods.DoubleTime | Mods.Hidden, replay.Mods);
+        }
+
+        [Fact]
+        public void LoadFull()
+        {
+            var replay = new Replay();
+
+            replay.Load(TestResources.GetResourceStream("aetrna - 07th Expansion - rog-unlimitation [AngelHoney] (2020-07-24) Osu.osr"), true);
+
+            // Assert.NotNull(replay.Lifebar);      If lifebar is null, it don't fall below his max value (1)
+            Assert.NotNull(replay.Data);
+        }
+    }
+}
--- Circles/Replays/Replay.cs
+++ Circles/Replays/Replay.cs
@@ -3,6 +3,11 @@
 using Circles.Rulesets;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+using LzmaDecoder = SevenZip.Compression.LZMA.Decoder;
 
 namespace Circles.Replays
 {
@@ -110,5 +115,142 @@ namespace Circles.Replays
         /// Only present if <see cref="Mods.Target"/> is enabled.
         /// </remarks>
         public double? TotalAccuracy { get; set; }
+
+        /// <summary>
+        /// On replays set on version 20130319 or later, the 32-bit integer RNG seed used for the score will be encoded into an additional replay frame at the end of the LZMA stream
+        /// </summary>
+        public int? Seed { get; set; }
+
+        /// <summary>
+        /// Loads osu! replay from given stream.
+        /// </summary>
+        public void Load(string filename, bool fullLoad = false)
+        {
+            var reader = new BinaryReader(new FileStream(filename, FileMode.Open, FileAccess.Read));
+
+            try
+            {
+                Load(reader, fullLoad);
+            }
+            finally
+            {
+                reader.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Loads osu! replay from given stream.
+        /// </summary>
+        public void Load(Stream inStream, bool fullLoad = false)
+        {
+            var reader = new BinaryReader(inStream);
+
+            try
+            {
+                Load(reader, fullLoad);
+            }
+            finally
+            {
+                reader.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Loads osu! replay from specified <see cref="BinaryReader"/>.
+        /// </summary>
+        public void Load(BinaryReader reader, bool fullLoad = false)
+        {
+            Ruleset = (Ruleset)reader.ReadByte();
+            Version = reader.ReadInt32();
+            BeatmapHash = reader.ReadByte() == 0x0B ? reader.ReadString() : null;
+            Username = reader.ReadByte() == 0x0B ? reader.ReadString() : null;
+            Hash = reader.ReadByte() == 0x0B ? reader.ReadString() : null;
+            Count300 = reader.ReadUInt16();
+            Count100 = reader.ReadUInt16();
+            Count50 = reader.ReadUInt16();
+            Gekis = reader.ReadUInt16();
+            Katus = reader.ReadUInt16();
+            Misses = reader.ReadUInt16();
+            TotalScore = reader.ReadUInt32();
+            MaxCombo = reader.ReadUInt16();
+            IsPerfect = reader.ReadBoolean();
+            Mods = (Mods)reader.ReadInt32();
+
+            if (fullLoad)
+            {
+                string lifedata = reader.ReadByte() == 0x0B ? reader.ReadString() : null;
+
+                if (!string.IsNullOrEmpty(lifedata))
+                {
+                    var frames = new List<LifebarFrame>();
+
+                    foreach (string block in lifedata.Split(','))
+                    {
+                        var pair = block.Split('|');
+
+                        if (pair.Length == 2)
+                        {
+                            frames.Add(new LifebarFrame
+                            {
+                                Time = int.Parse(pair[0]),
+                                Value = float.Parse(pair[1], CultureInfo.InvariantCulture)
+                            });
+                        }
+                    }
+
+                    Lifebar = frames.ToArray();
+                }
+            }
+
+            SubmitDate = new DateTime(reader.ReadInt64(), DateTimeKind.Utc);
+
+            if (fullLoad)
+            {
+                var frames = new List<DataFrame>();
+
+                foreach (string frame in Encoding.UTF8.GetString(LzmaDecoder.Decompress(reader.ReadBytes(reader.ReadInt32()))).Split(','))
+                {
+                    if (string.IsNullOrEmpty(frame))
+                    {
+                        break;
+                    }
+
+                    var pair = frame.Split('|');
+
+                    if (pair.Length == 4)
+                    {
+                        if (pair[0] == "-12345")
+                        {
+                            Seed = int.Parse(pair[3]);
+
+                            break;
+                        }
+
+                        frames.Add(new DataFrame
+                        {
+                            Time = int.Parse(pair[0]),
+                            X = float.Parse(pair[1], CultureInfo.InvariantCulture),
+                            Y = float.Parse(pair[2], CultureInfo.InvariantCulture),
+                            Actions = (Actions)byte.Parse(pair[3])
+                        });
+                    }
+                }
+
+                Data = frames.ToArray();
+            }
+            else
+            {
+                int length = reader.ReadInt32();
+
+                reader.BaseStream.Position += length;
+            }
+
+            ScoreId = reader.ReadInt64();
+
+            if (Mods.HasFlag(Mods.Target))
+            {
+                TotalAccuracy = reader.ReadDouble();
+            }
+        }
     }
 }
