cd55ae3f1b65271fd6692cf76f31381b36314b12
Redpoll <dredpoll02@gmail.com>
2021-04-28 22:09:01 +0500

    Add binary reader/writer for osu string type

---
+++ Circles/OsuBinaryReader.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2021 02Redpoll. See the MIT license for full information
+
+using System.IO;
+using System.Text;
+
+namespace Circles
+{
+    public sealed class OsuBinaryReader : BinaryReader
+    {
+        public OsuBinaryReader(Stream input) : base(input, new UTF8Encoding(), false)
+        {
+        }
+
+        public OsuBinaryReader(Stream input, Encoding encoding) : base(input, encoding, false)
+        {
+        }
+
+        public OsuBinaryReader(Stream input, Encoding encoding, bool leaveOpen) : base(input, encoding, leaveOpen)
+        {
+        }
+
+        public override string ReadString()
+        {
+            if (ReadByte() == 0x0B)
+            {
+                return base.ReadString();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Circles/OsuBinaryWriter.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2021 02Redpoll. See the MIT license for full information
+
+using System.IO;
+using System.Text;
+
+namespace Circles
+{
+    public sealed class OsuBinaryWriter : BinaryWriter
+    {
+        public OsuBinaryWriter(Stream input) : base(input, new UTF8Encoding(), false)
+        {
+        }
+
+        public OsuBinaryWriter(Stream input, Encoding encoding) : base(input, encoding, false)
+        {
+        }
+
+        public OsuBinaryWriter(Stream input, Encoding encoding, bool leaveOpen) : base(input, encoding, leaveOpen)
+        {
+        }
+
+        public override void Write(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Write(0x00);
+            }
+            else
+            {
+                Write(0x0B);
+                base.Write(value);
+            }
+        }
+    }
+}
